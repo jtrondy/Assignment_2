78ec44cc8a0f2930389e915aaad08440b3dbe951
Jared <73672469+jtrondy@users.noreply.github.com>
2022-03-20 11:40:49 +1300

    slideshow implemented - source credited

---
+++ slideshow.js
@@ -0,0 +1,13 @@
+// N/A[Coding Journey].(2019, June 17). Simple-Auto-Playing Image Slideshow (HTML, CSS and JavaScript)[video].YouTube.
+//     https://youtu.be/o_hRhTZORNY
+const slides = document.querySelectorAll(".slide");
+let currentImage = 0;
+
+slides[currentImage].style.display = "block";
+
+setInterval(nextImage, 3000);
+function nextImage() {
+    slides[currentImage].style.display = "none";
+    currentImage = (currentImage + 1) % slides.length;
+    slides[currentImage].style.display = "block";
+}
