73fd52733da9dc24b5e0745211769c2e1d5f0ec3
Jared Stevenson <73672469+jtrondy@users.noreply.github.com>
2022-03-31 00:28:43 +1300

    refactoring of id/class names, CSS file organised

--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -1,6 +1,6 @@
 // N/A[Coding Journey].(2019, June 17). Simple-Auto-Playing Image Slideshow (HTML, CSS and JavaScript)[video].YouTube.
 //     https://youtu.be/o_hRhTZORNY
-const slides = document.querySelectorAll(".slide");
+const slides = document.querySelectorAll(".index_slide");
 let currentImage = 0;
 
 slides[currentImage].style.display = "block";
