9df1100b7abe407c24807077413c112f9f743e2f
Jared Stevenson <73672469+jtrondy@users.noreply.github.com>
2022-03-31 00:33:30 +1300

    comment, removed redundant code

--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -1,11 +1,13 @@
 // N/A[Coding Journey].(2019, June 17). Simple-Auto-Playing Image Slideshow (HTML, CSS and JavaScript)[video].YouTube.
 //     https://youtu.be/o_hRhTZORNY
+
+
 const slides = document.querySelectorAll(".index_slide");
 let currentImage = 0;
 
-slides[currentImage].style.display = "block";
-
+//repeatedly calls function nextImage every 3 seconds
 setInterval(nextImage, 3000);
+
 function nextImage() {
     slides[currentImage].style.display = "none";
     currentImage = (currentImage + 1) % slides.length;
