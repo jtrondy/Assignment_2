157eebe9845a702f352a28ade74a408d997564a3
thyrealleftshark <christopher@fingersoffire.com>
2022-04-01 11:40:46 +1300

    Added footer.js file and added footer reference to each page

---
+++ scripts/footer.js
@@ -0,0 +1,11 @@
+const template = document.createElement('template');
+
+template.innerHTML = `
+    <footer class="index_footer">
+        <p>
+            COPYRIGHT © 32-BIT REPAIR // <a href="#">COPYRIGHT & DISCLAIMER</a> // <a href="#">PRIVACY POLICY</a> // <a href="pages/contactUs.html">CONTACT US</a>
+        </p>
+    </footer>
+`;
+
+document.body.appendChild(template.content);
