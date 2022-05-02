d3d07369b2082af11e065a276e9dcd9cb40b5898
codlpam <cody-pamment@outlook.com>
2022-05-03 09:56:28 +1200

    added javascript header

---
+++ scripts/header.js
@@ -0,0 +1,10 @@
+document.write('<header>' +
+    '<nav id="index_nav_bar">' +
+    '<a href="../index.html"><img src="../images/icons_logos/LOGO.png" alt="32-bit repair business logo"></a>' +
+    '<a href="../pages/service.html">SERVICES</a>' +
+    '<a href="../pages/booking.html">BOOKING</a>' +
+    '<a href="../pages/aboutUs.html">ABOUT</a>' +
+    '<a href="../pages/contactUs.html">CONTACT</a>' +
+    '<a href="../pages/customerFeedback.html">FEEDBACK</a>' +
+    '</nav>' +
+    '</header>')
